b13263d83027bc7b96d77400690a3ef28ce3f17f
JADPA-31 <JADPA-31@DESKTOP-6VO67MQ>
2023-08-17 10:21:08 -0600

    Agregar archivos de proyecto.

---
+++ WinFormsApp1/activity.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WinFormsApp1
+{
+    partial class activity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnsafe = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.lsta = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::WinFormsApp1.Properties.Resources.descarga;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(745, 173);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Actividad";
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(12, 214);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(130, 23);
+            this.btnadd.TabIndex = 3;
+            this.btnadd.Text = "Añadir Actividad";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnsafe
+            // 
+            this.btnsafe.Location = new System.Drawing.Point(439, 214);
+            this.btnsafe.Name = "btnsafe";
+            this.btnsafe.Size = new System.Drawing.Size(75, 23);
+            this.btnsafe.TabIndex = 4;
+            this.btnsafe.Text = "Guardar";
+            this.btnsafe.UseVisualStyleBackColor = true;
+            this.btnsafe.Click += new System.EventHandler(this.btnsafe_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(551, 214);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 5;
+            this.btndelete.Text = "Eliminar";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(661, 214);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(75, 23);
+            this.btncancel.TabIndex = 6;
+            this.btncancel.Text = "Cancelar";
+            this.btncancel.UseVisualStyleBackColor = true;
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(82, 280);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(654, 23);
+            this.txta.TabIndex = 7;
+            this.txta.TextChanged += new System.EventHandler(this.txta_TextChanged);
+            // 
+            // lsta
+            // 
+            this.lsta.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lsta.FormattingEnabled = true;
+            this.lsta.ItemHeight = 15;
+            this.lsta.Location = new System.Drawing.Point(25, 326);
+            this.lsta.Name = "lsta";
+            this.lsta.Size = new System.Drawing.Size(711, 154);
+            this.lsta.TabIndex = 8;
+            // 
+            // activity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 492);
+            this.Controls.Add(this.lsta);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnsafe);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "activity";
+            this.Text = "Agenda de actividad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.activity_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Button btnadd;
+        private Button btnsafe;
+        private Button btndelete;
+        private Button btncancel;
+        private TextBox txta;
+        private ListBox lsta;
+    }
+}
---
+++ WinFormsApp1/activity.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class activity : Form
+    {
+        private bool _Haschange;
+        private bool _Isnewact;
+        public activity()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            this.lsta.Enabled = false;
+            this.btndelete.Enabled = false;
+            this.btnsafe.Enabled = false;
+            this.btncancel.Enabled = false;
+            this.btnadd.Enabled = true;
+            this.txta.Enabled = false;
+
+            //enable/disable the listbox based on number items
+            this.lsta.Enabled = this.lsta.Items.Count > 0;
+            this.lsta.SelectedIndex = -1;
+
+            _Haschange = false;
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            this.Addnewa();
+        }
+
+        private void Addnewa()
+        {
+            if (_Haschange) {
+                if (MessageBox.Show("Guardar cambios?", "Guardar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+                    if (!this.SaveChanges()) {
+                        return;
+                    }
+                }
+            }
+            btncancel.Enabled = true;
+            btnadd.Enabled = true;
+            btnsafe.Enabled = true;
+
+            txta.Enabled = true;
+            txta.Clear();
+            txta.Focus();
+
+            _Isnewact = true;
+        }
+
+        private bool SaveChanges()
+        {
+            if (txta.Text.Length == 0) {
+                MessageBox.Show("Debe escribir un nombre para la actividad", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (_Isnewact)
+            {
+                this.lsta.Items.Add(txta.Text);
+                this.Reset();
+            }
+            else
+            {
+                this.lsta.Items[lsta.SelectedIndex] = txta.Text;
+                MessageBox.Show("Guardado correctamente");
+            }
+            return true;
+        }
+
+        private void btnsafe_Click(object sender, EventArgs e)
+        {
+            this.SaveChanges();
+        }
+
+        private void activity_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult r = MessageBox.Show("¿Guardar cambios?", "Guardar", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+            if (r == DialogResult.Yes)
+            {
+                if (this.SaveChanges())
+                {
+
+                    e.Cancel = true;
+                    return;
+                }
+            }
+            else if (r == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            this.deleteact();
+        }
+
+        private void deleteact()
+        {
+            if (MessageBox.Show("Desea eliminar la actividad?", "Confirmar Eliminacion", MessageBoxButtons.YesNo) == DialogResult.Yes) {
+                if(lsta.SelectedIndex >=0 && lsta.SelectedIndex < lsta.SelectedItems.Count)
+                {
+                    this.lsta.Items.RemoveAt(lsta.SelectedIndex);
+                    Reset();
+                }
+            }
+        }
+
+        private void txta_TextChanged(object sender, EventArgs e)
+        {
+            _Haschange = true;
+        }
+    }
+}
